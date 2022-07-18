99216a9644fd9f0b349e615fca308b21855f3eac
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-07-18 19:21:56 -0300

    Requisito 02

---
+++ src/database/models/user.js
@@ -0,0 +1,14 @@
+const sequelize = require('sequelize');
+
+const createUser = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+  return User;
+};
+
+module.exports = createUser;
