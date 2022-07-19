8afc727433f499572acc3917b54e25c67809039e
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-07-19 20:12:34 -0300

    Requisito 03

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -2,9 +2,13 @@ const authService = require('../services/authService');
 
 const authController = {
   async login(req, res) {
-    const { email, password } = req.body;
-    const token = await authService.login(email, password);
-    res.status(200).json({ token });
+    try {
+      const { email, password } = req.body;
+      const token = await authService.login(email, password);
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(error.code).json({ message: error.message });
+    }
   },
 };
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -2,8 +2,18 @@ const { User } = require('../database/models');
 const jwtService = require('./jwtService');
 
 const authService = {
-  async login(email, _password) {
+  async login(email, password) {
+    if (!email || !password) {
+      const error = new Error('Some required fields are missing');
+      error.code = 400;
+      throw error;
+    }
     const user = await User.findOne({ where: { email } });
+    if (!user || user.password !== password) {
+      const error = new Error('Invalid fields');
+      error.code = 400;
+      throw error;
+    }
     const token = jwtService.createToken(user);
     return token;
   },
