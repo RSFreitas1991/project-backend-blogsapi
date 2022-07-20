6ee5667e0db3ff71a1d196f4229114e921fee9c0
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-07-20 15:32:25 -0300

    Token sendo criado somente com o displayName do User

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,19 +2,19 @@ const { User } = require('../database/models');
 const jwtService = require('./jwtService');
 
 const authService = {
-  async login(email, password) {
-    if (!email || !password) {
+  async login(email, passwords) {
+    if (!email || !passwords) {
       const error = new Error('Some required fields are missing');
       error.code = 400;
       throw error;
     }
     const user = await User.findOne({ where: { email } });
-    if (!user || user.password !== password) {
+    if (!user || user.password !== passwords) {
       const error = new Error('Invalid fields');
       error.code = 400;
       throw error;
     }
-    const token = jwtService.createToken(user);
+    const token = jwtService.createToken(user.displayName);
     return token;
   },
 };
