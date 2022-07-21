89871f639d05982e1e5e4934886445095adcb596
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-07-21 20:15:42 -0300

    Requisito 11

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,40 @@
+const sequelize = require('sequelize');
+
+const createPostCategory = (sequelize, DataTypes) => {
+  const postCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      references: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      }
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      references: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      }
+    },
+  }, {
+    tableName: 'PostCategories',
+    timestamps: false,
+  });
+  postCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'BlogPost',
+      through: postCategory,
+      foreignKey: 'id',
+      OtherKey: 'id'
+    })
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'Category',
+      through: postCategory,
+      foreignKey: 'id',
+      OtherKey: 'id'
+    })
+  };
+  return postCategory;
+};
+
+module.exports = createPostCategory;
